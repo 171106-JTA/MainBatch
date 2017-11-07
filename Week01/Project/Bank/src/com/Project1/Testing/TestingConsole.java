c03913f2de4b3ff77ad8965576dc653378e9b7f8
Evan West <evan-west@hotmail.com>
2017-11-07 13:32:08 -0500

    Basic user input

--- Week01/Project/Bank/src/com/Project1/Testing/TestingConsole.java
+++ Week01/Project/Bank/src/com/Project1/Testing/TestingConsole.java
@@ -1,10 +1,19 @@
 package com.Project1.Testing;
 
+import java.util.Scanner;
+
 public class TestingConsole {
 
-	public static void main(String[] args) {		
-		//Simple function call!
-		System.out.println("Hello Console!!!");
+	public static void main(String[] args) {			
+		//Get and print user input
+		//Following tutorial: 
+		//http://www.programmingsimplified.com/java/source-code/java-program-take-input-from-user
+		//Accessed 11/7/2017
+		
+		Scanner in = new Scanner(System.in); 
+		System.out.println("Enter a string: ");
+		String s = in.nextLine();
+		System.out.println(s);
 
 	}
 
