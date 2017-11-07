7a35affc141b55842b9102cb86f52b953abd59db
Evan West <evan-west@hotmail.com>
2017-11-07 13:26:56 -0500

    Basic application, Run through console

---
+++ Week01/Project/Bank/src/com/Project1/Testing/TestingConsole.java
@@ -0,0 +1,11 @@
+package com.Project1.Testing;
+
+public class TestingConsole {
+
+	public static void main(String[] args) {		
+		//Simple function call!
+		System.out.println("Hello Console!!!");
+
+	}
+
+}
