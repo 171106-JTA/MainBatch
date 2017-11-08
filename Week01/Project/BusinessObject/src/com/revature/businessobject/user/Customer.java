d8ae316c21941e636b50e1eac42ca899bcfc20fc
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-07 22:56:44 -0500

    Added Customer Class

---
+++ Week01/Project/BusinessObject/src/com/revature/businessobject/user/Customer.java
@@ -0,0 +1,15 @@
+package com.revature.businessobject.user;
+
+/**
+ * Defines User with customer privileges 
+ * @author Antony Lulciuc
+ */
+public class Customer extends User {
+	/**
+	 * Creates user with customer privileges 
+	 * @param id user primary key
+	 */
+	public Customer(long id) {
+		super(id, UserRole.CUSTOMER);
+	}
+}
