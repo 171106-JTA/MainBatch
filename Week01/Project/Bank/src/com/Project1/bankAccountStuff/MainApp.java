a80e902dcbc7045b15c16c3f9c082d8ce73b6fba
Evan West <evan-west@hotmail.com>
2017-11-07 21:01:52 -0500

    Initial Psuedocode for project. This is a work in Progress

---
+++ Week01/Project/Bank/src/com/Project1/bankAccountStuff/MainApp.java
@@ -0,0 +1,64 @@
+package com.Project1.bankAccountStuff;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Just Saying Something");
+	}
+	
+	/**
+	 * Allows user to login. 
+	 */
+	private void login() { 
+		
+	}
+	
+	/**
+	 * Allows user to create a new client account
+	 */
+	private void createNewClientAccount() {
+		
+	}
+	
+	/**
+	 * Displays initial menu
+	 */
+	private void displayLoginMenu() {
+		
+	}
+	
+	/**
+	 * Get user's response to menus
+	 * @return returns user's choice
+	 */
+	private int getUserResponse() {
+		return 0;
+	}
+	
+	/**
+	 * Display login menu, get user's choice, and validate user's choice
+	 * @return returns user's choice
+	 */
+	private int LoginMenu() {
+		return 0;
+	}
+	
+	/**
+	 * Display menu for Admins, get Admin's choice, and validate admin's choice
+	 */
+	private void AdminMenu() {
+		
+	}
+	
+	/**
+	 * Display menu for Client's, get Clien'ts choice, and validate Client's choice
+	 */
+	private void ClientMenu() {
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Week01/Project/Bank/src/com/Project1/bankAccountStuff/User.java
@@ -0,0 +1,18 @@
+package com.Project1.bankAccountStuff;
+
+public class User {
+	/**
+	 * Username of Client
+	 */
+	protected String name; 
+	
+	/**
+	 * Password of Client
+	 */
+	protected String password;
+	
+	/**
+	 * Holds level of permissions for user. 0 = Client, 1 = Admin
+	 */
+	protected int permissions;
+}
