b72a861af5a24c1794da21823dc1c213c1520c38
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-08 20:37:07 -0500

    Implemented FilePersistenceTest stubs

--- Week01/Project/SystemTests/src/com/revature/test/persistence/FilePersistenceTest.java
+++ Week01/Project/SystemTests/src/com/revature/test/persistence/FilePersistenceTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import com.revature.businessobject.user.Admin;
 import com.revature.businessobject.user.Customer;
 import com.revature.businessobject.user.User;
+import com.revature.businessobject.user.UserRole;
 import com.revature.core.FieldParams;
 import com.revature.core.Resultset;
 import com.revature.persistence.Persistenceable;
@@ -21,6 +22,9 @@ import com.revature.persistence.file.FilePersistence;
 
 public class FilePersistenceTest {
 	static Persistenceable manager;
+	static Resultset resultset;
+	static FieldParams conditions;
+	static FieldParams values;
 	static User admin;
 	static User customer;
 	static long adminId;
@@ -35,6 +39,9 @@ public class FilePersistenceTest {
 	@Before
 	public void setUp() throws Exception {
 		manager = new FilePersistence();
+		resultset = new Resultset();
+		conditions = new FieldParams();
+		values = new FieldParams();
 		admin = new Admin(adminId);
 		customer = new Customer(customerId);
 	}
@@ -61,51 +68,90 @@ public class FilePersistenceTest {
 	///
 	
 	/**
-	 * Creates and attempts to get user from file
+	 * Creates admin account using BusinessObject
 	 */
 	@Test
-	public void shouldGetUser() {
-		FieldParams cnds = new FieldParams();
-		Resultset resultset;
-		
-		// Set condition to pull from manager
-		cnds.put("id", Long.toString(adminId));
+	public void shouldCreateNewAdminUserUsingBusinessObject() {
+		assertEquals("Should add admin with", manager.insert(admin), 1);
+	}
+	
+	/**
+	 * Creates admin account using FieldParams
+	 */
+	@Test
+	public void shouldCreateNewAdminUserUsingFieldParams() {
+		// Set data
+		values.put("id", Long.toString(adminId));
+		values.put("role", admin.getRole().toString());
 		
 		// Perform test
-		assertEquals("Should add admin with", manager.insert(admin), 1);
-		assertNotNull("Should get handle to list of business objects", resultset = manager.select("User", cnds));
+		assertEquals("Should add admin with", manager.insert("User", values), 1);
+	}
+	
+	@Test
+	public void shouldBeAbleToGetExistingUser() {
+		// Set condition to pull from manager
+		conditions.put("id", Long.toString(adminId));
+		manager.insert(admin);
+		
+		// Perform test
+		assertNotNull("Should get handle to list of business objects", resultset = manager.select("User", conditions));
 		assertEquals("Should have one record in resultset", resultset.size(), 1);
 		assertTrue("Record should be newly created admin", admin.equals(resultset.get(0)));
 	}
 	
+	/**
+	 * Attempts to update an existing record using instance of BusinessObject
+	 */
 	@Test
 	public void shouldUpdateUserByBusinessObject() {
+		User demote = new Customer(adminId);
 		
+		// Set data
+		conditions.put("id", Long.toString(adminId));
+		manager.insert(admin);
 		
+		// Update account (demote to customer)
+		assertEquals("Should demote admin account to customer", manager.update(demote), 1);
+		assertNotNull("Should be able to get updated record", resultset = manager.select("User", conditions));
+		assertEquals("Should have one record in resultset", resultset.size(), 1);
+		assertTrue("Record in system should reflect changes", demote.equals(resultset.get(0)));
 	}
 
+	/**
+	 * Attempts to update an existing record using FieldParams instance 
+	 */
 	@Test
-	public void shouldUpdateUserByParams() {
-		fail("Not implemented");
-	}
-	
-	@Test
-	public void shouldInsertUserByBusinessObject() {
-		fail("Not implemented");
-	}
-	
-	@Test
-	public void shouldInsertUserByParams() {
-		fail("Not implemented");
+	public void shouldUpdateUserByFieldParams() {
+		User demote = new Customer(adminId);
+		// Set data
+		conditions.put("id", Long.toString(adminId));
+		values.put("role", demote.getRole().toString());
+		manager.insert(admin);
+		
+		// Update Account
+		assertEquals("Should demote admin account to customer", manager.update("User", conditions, values), 1);
+		assertNotNull("Should be able to get updated record", resultset = manager.select("User", conditions));
+		assertEquals("Should have one record in resultset", resultset.size(), 1);
+		assertTrue("Record in system should reflect changes", demote.equals(resultset.get(0)));
 	}
 	
 	@Test
 	public void shouldRemoveUserByBusinessObject() {
-		fail("Not implemented");
+		// Set data
+		manager.insert(admin);
+		
+		// Perform test
+		assertEquals("Should delete admin account", manager.delete(admin), 1);
 	}
 	
 	@Test
 	public void shouldRemoveUserByParams() {
-		fail("Not implemented");
+		// Set data
+		conditions.put("id", Long.toString(adminId));
+		manager.insert(admin);
+		
+		// Perform test
+		assertEquals("Should delete admin account", manager.delete("User", conditions), 1);
 	}
 }
