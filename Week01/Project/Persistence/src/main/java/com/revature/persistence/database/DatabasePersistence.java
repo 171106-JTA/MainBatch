afc82ec72819fefbc32a5b4902580aac9a7559c7
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-14 14:36:55 -0500

    Added jdbc dependency -Created package com.revature.persistence.database -Added abstract class DatabasePersistence

---
+++ Week01/Project/Persistence/src/main/java/com/revature/persistence/database/DatabasePersistence.java
@@ -0,0 +1,8 @@
+package com.revature.persistence.database;
+
+import com.revature.persistence.Persistenceable;
+import java.sql.*;
+
+public abstract class DatabasePersistence implements Persistenceable {
+
+}
