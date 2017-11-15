df3abc0f1b5fb57ce981f061558062a170bf9c57
Karandeep Saini <kpsaini34@gmail.com>
2017-11-15 15:49:39 -0500

    commit to pull in class examples

--- Week01/_InClassExamples/171106_Java/src/d2/revature/fileio/FileIOExample.java
+++ Week01/_InClassExamples/171106_Java/src/d2/revature/fileio/FileIOExample.java
@@ -2,7 +2,6 @@ package d2.revature.fileio;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
--- Week01/_InClassExamples/171106_Java/src/d2/revature/pillars/SerializationExample.java
+++ Week01/_InClassExamples/171106_Java/src/d2/revature/pillars/SerializationExample.java
@@ -1,6 +1,5 @@
 package d2.revature.pillars;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
