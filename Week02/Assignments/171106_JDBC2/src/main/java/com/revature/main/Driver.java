830dc7b62189ee7261869746b8e3ffab2e420ad9
Nasir Alauddin <nalaudd@yahoo.com>
2017-11-17 15:25:41 -0500

    class assignment full directory submission

---
+++ Week02/Assignments/171106_JDBC2/src/main/java/com/revature/main/Driver.java
@@ -0,0 +1,34 @@
+package com.revature.main;
+
+import java.util.List;
+
+import com.revature.beans.FlashCard;
+import com.revature.dao.FlashCardDao;
+import com.revature.dao.FlashCardDaoImpl;
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception {
+		FlashCardDao dao = new FlashCardDaoImpl();
+		FlashCard fc = new FlashCard("Did JDBC work?", "Yes it did!");
+		
+		dao.createFlashCard(fc);
+		
+		System.out.println(dao.selectFlashCardById(1));
+		
+		dao.createFlashCardSP(new FlashCard("StoredProcedure Example", "Success!"));
+		
+		List<FlashCard> fcards = dao.getAllFlashCards();
+		
+		for(FlashCard f : fcards)
+			System.out.println(f);
+		
+		
+/*		fc = new FlashCard("A UNIQUE FLIPPIN QUESTION2", "Are A PAIN IN MY A!2");
+		dao.createFlashCard(fc);*/
+		/*
+		System.out.println(dao.getAnswerByQuestion(fc));*/
+		
+	}
+
+}
