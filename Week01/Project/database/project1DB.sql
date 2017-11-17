425db94197bd8bed6c088a6de7bd792a6c4dc36a
Evan West <evan-west@hotmail.com>
2017-11-17 13:03:16 -0500

    Create an insert procedure and test it in the SQL script

--- Week01/Project/database/project1DB.sql
+++ Week01/Project/database/project1DB.sql
@@ -4,12 +4,14 @@ CREATE TABLE A_USER(
     FIRSTNAME VARCHAR2(100),
     LASTNAME VARCHAR2(100),
     MIDDLEINITIAL VARCHAR2(1),
-    PASSWORD VARCHAR(100),
+    USER_PASSWORD VARCHAR(100),
     STATUS NUMBER(1),
     PERMISSION NUMBER(1), 
-    ACCOUNTAMOUNT NUMBER(38, 2)    
+    ACCOUNTAMOUNT NUMBER(38, 2), 
+    CONSTRAINT PK_a_user PRIMARY KEY(USERNAME)
 );
-
+/
+--Sample users, if needed
 --INSERT INTO A_USER 
 --VALUES('A', 'A', 'A','A','A',0,0,0);
 --INSERT INTO A_USER 
@@ -20,3 +22,31 @@ CREATE TABLE A_USER(
 --VALUES('D', 'D', 'D','D','D',0,0,0);
 --
 --SELECT * FROM A_USER;
+
+CREATE OR REPLACE PROCEDURE insert_user(
+    new_username IN VARCHAR2, 
+    new_firstname IN VARCHAR2, 
+    new_lastname IN VARCHAR2, 
+    new_middleinitial IN VARCHAR2,
+    new_user_password IN VARCHAR2, 
+    new_status IN NUMBER,
+    new_permission IN NUMBER,
+    new_accountamount IN NUMBER)
+IS
+BEGIN
+    INSERT INTO A_USER (USERNAME, FIRSTNAME, LASTNAME, MIDDLEINITIAL,
+                        USER_PASSWORD, STATUS, PERMISSION, ACCOUNTAMOUNT)
+    VALUES(new_username, new_firstname, new_lastname, new_middleinitial, 
+        new_user_password, new_status, new_permission, new_accountamount);
+END;
+/
+
+--Testing the insert_user procedure!
+DECLARE
+    TMP VARCHAR(5);
+BEGIN
+    TMP := 'A';
+    insert_user(TMP, TMP, TMP, TMP, TMP, 0, 0, 0);
+END;
+/
+SELECT * FROM A_USER;
