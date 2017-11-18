319ae8323cb5e7395245872ba91c5fc04db551b3
pibe2 <pibe2@illinois.edu>
2017-11-18 09:35:46 -0500

    removed old files

--- Week01/Project/src/main/java/daxterix/bank/dao/DAOUtils.java
+++ Week01/Project/src/main/java/daxterix/bank/dao/DAOUtils.java
@@ -1,47 +1,36 @@
 package daxterix.bank.dao;
 
-import daxterix.bank.dao.old.AdminDAO;
-import daxterix.bank.dao.old.CustomerDAO;
-import daxterix.bank.dao.old.RequestDAO;
-
 public class DAOUtils {
     /***
      * get data access object for basic CRUD operations for Admin records
      *
      * @return
      */
-    public static AdminDAO getAdminDao() {
-        //return new AdminDAO(adminPath);
-        return null;
+    public static UserDAO getUserDao() {
+        return new UserDAOImpl();
     }
 
-    /**
-     * get data access object for basic CRUD operations for unlocked Customer records
-     *
-     * @return
-     */
+    public static RequestDAO2 getRequestDao() {
+        return new RequestDAOImpl();
+    }
+
+    public static AccountDAO getAccountDao() {
+        return new AccountDAOImpl();
+    }
+
+
+    /*
     public static CustomerDAO getUnlockedCustomerDao() {
         //return new CustomerDAO(unlockedCustomerPath);
         return null;
     }
-
-    /**
-     * get data access object for basic CRUD operations for locked Customer records
-     *
-     * @return
-     */
     public static CustomerDAO getLockedCustomerDao() {
         //return new CustomerDAO(lockedCustomerPath);
         return null;
     }
-
-    /**
-     * get data access object for basic CRUD operations for CustomerRequest records
-     *
-     * @return
-     */
     public static RequestDAO getRequestDao() {
         //return new RequestDAO(requestPath);
         return null;
     }
+    */
 }
