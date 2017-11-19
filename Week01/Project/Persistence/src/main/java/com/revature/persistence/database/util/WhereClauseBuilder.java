ccaed9d8aa049b6287ceb8289faffd45678c69fc
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-18 22:51:29 -0500

    implemented WhereClauseBuilder method buildUserWhereClause

--- Week01/Project/Persistence/src/main/java/com/revature/persistence/database/util/WhereClauseBuilder.java
+++ Week01/Project/Persistence/src/main/java/com/revature/persistence/database/util/WhereClauseBuilder.java
@@ -1,12 +1,17 @@
 package com.revature.persistence.database.util;
 
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
+import com.revature.businessobject.user.User;
 import com.revature.core.BusinessClass;
 import com.revature.core.FieldParams;
 
 public class WhereClauseBuilder {
 	private static WhereClauseBuilder builder;
+	private static Logger logger = Logger.getLogger(WhereClauseBuilder.class);
 	
 	private WhereClauseBuilder() {
 		// do nothing
@@ -18,7 +23,7 @@ public class WhereClauseBuilder {
 	}
 
 	
-	public boolean build(String name, Statement statement, FieldParams params) {
+	public boolean build(String name, PreparedStatement statement, FieldParams params) {
 		switch (name.toLowerCase()) {
 			case BusinessClass.ADMIN:
 			case BusinessClass.CUSTOMER:
@@ -42,35 +47,59 @@ public class WhereClauseBuilder {
 	///
 	
 	
-	private boolean buildUserWhereClause(Statement statement, FieldParams params) {
+	private boolean buildUserWhereClause(PreparedStatement statement, FieldParams params) {
+		int pos = 1;
+		
+		for (String key : params.keySet()) {
+			try {
+				// Attempt to assemble where clause
+				switch (key) {
+					case User.ID:
+						statement.setLong(pos, Long.parseLong(params.get(key)));
+						break;
+					case User.USERNAME:
+					case User.PASSWORD:
+						statement.setString(pos, params.get(key));
+						statement.setString(pos, params.get(key));
+				}
+			} catch (NumberFormatException | SQLException e) {
+				logger.warn("failed to build user where clause, message=" + e.getMessage());
+				e.printStackTrace();
+				return false;
+			}
+			
+			// increment counter
+			++pos;
+		}
+		
+		return true;
+	}
+	
+	private boolean buildUserInfoWhereClause(PreparedStatement statement, FieldParams params) {
+		return true;
+	}
+	
+	private boolean buildAccountWhereClause(PreparedStatement statement, FieldParams params) {
 		return false;
 	}
 	
-	private boolean buildUserInfoWhereClause(Statement statement, FieldParams params) {
+	private boolean buildCheckingWhereClause(PreparedStatement statement, FieldParams params) {
 		return false;
 	}
 	
-	private boolean buildAccountWhereClause(Statement statement, FieldParams params) {
+	private boolean buildCreditWhereClause(PreparedStatement statement, FieldParams params) {
 		return false;
 	}
 	
-	private boolean buildCheckingWhereClause(Statement statement, FieldParams params) {
+	private boolean buildSavingsWhereClause(PreparedStatement statement, FieldParams params) {
 		return false;
 	}
 	
-	private boolean buildCreditWhereClause(Statement statement, FieldParams params) {
+	private boolean buildCodeListWhereClause(PreparedStatement statement, FieldParams params) {
 		return false;
 	}
 	
-	private boolean buildSavingsWhereClause(Statement statement, FieldParams params) {
-		return false;
-	}
-	
-	private boolean buildCodeListWhereClause(Statement statement, FieldParams params) {
-		return false;
-	}
-	
-	private booleans buildReceiptWhereClause(Statement statment, FieldParams params) {
+	private boolean buildReceiptWhereClause(PreparedStatement statment, FieldParams params) {
 		return false;
 	}
 	
