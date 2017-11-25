549b264f713fac77551792052cc8008a70788667
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-25 13:49:40 -0500

    Refactored BusinessObject interface now DAOBusinessObject class -Added ERD -Added sql scripts to generate tables -Added sequence and triggers for auto incrementation

--- Projects/Project1/DAO/src/main/java/com/revature/dao/DAOBusinessObject.java
+++ Projects/Project1/DAO/src/main/java/com/revature/dao/DAOBusinessObject.java
@@ -2,38 +2,52 @@ package com.revature.dao;
 
 import java.util.List;
 
+import com.revature.businessobject.BusinessObject;
+
 /**
- * Interface used to manage data 
+ * Communication medium between database and server
  * @author Antony Lulciuc
  * @param <T> Java representation of Database Table
  */
-public interface BusinessObject<T> {
+public final class DAOBusinessObject {
 	/**
 	 * Acquires all records of represented type from database
 	 * @return all records of represented type
 	 */
-	public List<T> loadAll();
+	public static List<BusinessObject> loadAll() {
+		// stub
+		return null;
+	}
 	
 	/**
 	 * Loads items which matches argument (null values ignored)
 	 * @param item - what to check records against
 	 * @return list of records which met the criteria specified by argument 
 	 */
-	public List<T> load(T item);
+	public List<BusinessObject> load(BusinessObject item) {
+		// stub
+		return null;
+	}
 	
 	/**
 	 * Adds set of items to database
 	 * @param items - what to save in database
 	 * @return number of records created 
 	 */
-	public int insert(List<T> items);
+	public int insert(List<BusinessObject> items) {
+		// stub
+		return 0;
+	}
 	
 	/**
 	 * Adds item to database
 	 * @param item - what to save in database
 	 * @return 1 if item added to database else 0
 	 */
-	public int insert(T item);
+	public int insert(BusinessObject item) {
+		// stub
+		return 0;
+	}
 	
 	/**
 	 * Attempts to update existing record
@@ -41,19 +55,28 @@ public interface BusinessObject<T> {
 	 * @param value - new values for record
 	 * @return number of records updated 
 	 */
-	public int update(T item, T value);
+	public int update(BusinessObject item, BusinessObject value) {
+		// stub
+		return 0;
+	}
 	
 	/**
 	 * Deletes set of records from database
 	 * @param items - list of items to delete
 	 * @return number of records removed from database
 	 */
-	public int delete(List<T> items);
+	public int delete(List<BusinessObject> items) {
+		// stub
+		return 0;
+	}
 	
 	/**
 	 * Deletes item from database
 	 * @param item - what to delete
 	 * @return number of records removed from database
 	 */
-	public int delete(T item);
+	public int delete(BusinessObject item) {
+		// stub
+		return 0;
+	}
 }
