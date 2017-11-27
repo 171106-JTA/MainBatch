5a0ccb29dea29d5a086ed769ca30bc8c9518c039
antony lulciuc <antony.lulciuc@gmail.com>
2017-11-27 16:24:51 -0500

    renamed project Server to Service -renamed ServerUtil to ServiceUtil -added condition ServiceUtil methods such that class specified must implement BusinessObject

---
+++ Projects/Project1/EAR/src/main/java/com/revature/ear/controller/FrontController.java
@@ -0,0 +1,37 @@
+package com.revature.ear.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- Projects/Project1/Server/src/main/java/com/revature/service/util/ServiceUtil.java
+++ Projects/Project1/Server/src/main/java/com/revature/service/util/ServiceUtil.java
@@ -1,29 +1,30 @@
-package com.revature.server.util;
+package com.revature.service.util;
 
 import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.revature.businessobject.BusinessObject;
 
-public final class ServerUtil {
+public final class ServiceUtil {
 	private static Gson gson = new Gson();
 	
 	/**
-	 * Converts object to JSON using Gson Library
+	 * Converts object to JSON using Gson Library (Class must implement BusinessObject)
 	 * @param item - what to convert to json
 	 * @return json string representation of item 
 	 */
-	public static <T> String toJson(T item) {
+	public static <T extends BusinessObject> String toJson(T item) {
 		Type type = new TypeToken<T>() {}.getType(); 
 		return gson.toJson(item, type);
 	}
 	
 	/**
-	 * Converts json to java object
+	 * Converts json to java object (Class must implement BusinessObject)
 	 * @param json - what to transform to java
 	 * @return Java representation of json
 	 */
-	public static <T> T toJava(String json) {
+	public static <T extends BusinessObject> T toJava(String json) {
 		Type type = new TypeToken<T>() {}.getType(); 
 		return gson.fromJson(json, type);
 	}
