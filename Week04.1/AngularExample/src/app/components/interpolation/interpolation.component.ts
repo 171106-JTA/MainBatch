77191cacfbbbcadafa915183ea6b36234ee33c0f
Jeffrey Myers <jeffm.ii@outlook.com>
2017-12-01 14:00:36 -0500

    Changing directory name

---
+++ Week04.1/AngularExample/src/app/components/interpolation/interpolation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-interpolation",
+    templateUrl: "./interpolation.component.html"
+})
+
+export class InterpolationComponent{
+    componentvariable = "Blah";
+}
