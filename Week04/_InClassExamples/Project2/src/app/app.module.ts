e3b7749e99b8c2a9f214a7ab14a86065b92a22ba
Sean Vaeth <seanaveth@gmail.com>
2017-12-01 11:05:04 -0500

    commit

--- Week04/_InClassExamples/Project2/src/app/app.module.ts
+++ Week04/_InClassExamples/Project2/src/app/app.module.ts
@@ -1,34 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
-//Import all components used by the Angular application here
+
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { InterpolationComponent } from './components/interpolation/interpolation.component';
-import { appRoutes } from './routing';
-import { HomeComponent } from './components/home/home.component';
-import { PokeapiComponent } from './components/pokeapi/pokeapi.component';
 
 
 @NgModule({
-  //Have angular declare all classes to be used
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    InterpolationComponent,
-    HomeComponent,
-    PokeapiComponent
-
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule
-    
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
