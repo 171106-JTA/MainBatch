36938c29faa73f0789f52f39658c1d9a285d2865
Michael Tseng <michael-tseng@outlook.com>
2017-12-08 23:39:01 -0500

    implemented blob example

--- Projects/Project1/TRMS/src/main/java/com/banana/servlet/SubmitAdditionalInfo.java
+++ Projects/Project1/TRMS/src/main/java/com/banana/servlet/SubmitAdditionalInfo.java
@@ -1,15 +1,17 @@
 package com.banana.servlet;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 
 /**
@@ -17,22 +19,43 @@ import javax.servlet.http.HttpSession;
  */
 @MultipartConfig
 public class SubmitAdditionalInfo extends HttpServlet {
-	
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	/*
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-	}
+		//TODO get info
+	} */
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		
+		try {
+			Part p1 = request.getPart("file1");
+			InputStream is = p1.getInputStream();
+			System.out.println("here");
+			Part p2 = request.getPart("info");
+			Scanner s = new Scanner(p2.getInputStream());
+			String name = s.nextLine();
+			System.out.println(name);
+			String outputfile = p1.getName();
+			FileOutputStream os = new FileOutputStream(outputfile);
+			
+			int ch = is.read();
+			while(ch != -1) {
+				os.write(ch);
+				ch = is.read();
+			}
+			os.close();
+			out.println("<h3>Success!</h3>");
+		}catch(Exception e) {
+			out.println(e.getMessage() + "Exception");
+		}finally {
+			out.close();
+		}
 	}
 
 }
